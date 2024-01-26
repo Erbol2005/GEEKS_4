366b5a0a011133985bff0cfc40cbe4482721213e
Erbol2005 <ebokator@gmail.com>
2024-01-26 14:23:30 +0600

    HW_3

--- tekken_persons/views.py
+++ tekken_persons/views.py
@@ -6,11 +6,9 @@ def persons_list(request):
     if request.method == 'GET':
         persons = models.PersonGame.objects.all()
         return render(request, template_name='tekken_game/persons_list.html',
-<<<<<<< HEAD
                       context={'persons': persons})
-=======
-                      context={'persons': persons} )
->>>>>>> 071e9cfa7983c41df8fa0223e0105b965cce1eba
+
+
 
 
 def persons_detail(reqvest, id):
